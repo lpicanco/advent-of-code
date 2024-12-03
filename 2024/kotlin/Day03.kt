4e35170293ef908918ca3fc5dee6654706c34150
lpicanco <lpicanco@gmail.com>
2024-12-03 08:30:28 -0300

    2024: day 03

---
+++ 2024/kotlin/Day03.kt
@@ -0,0 +1,45 @@
+package aoc2024
+
+import java.io.File
+
+class Day03(
+    private val input: String,
+) {
+    fun solve1(): Int {
+        return execute(input)
+    }
+
+    fun solve2(): Int {
+        val filterRegex = Regex("""don't\(\)[\s\S]*?do\(\)""")
+        return execute(input.replace(filterRegex, ""))
+    }
+
+    private fun execute(instructions: String): Int {
+        val regex = Regex("""mul\(\d{1,3},\d{1,3}\)""")
+        return regex.findAll(instructions).sumOf {
+            val (a, b) = it.value.drop(4).dropLast(1).split(",").map { it.toInt() }
+            a * b
+        }
+    }
+}
+
+fun main() {
+    val testInput1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    val testInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    Day03(testInput1).solve1().also {
+        check(it == 161) { "Invalid: $it" }
+        println(it)
+    }
+    Day03(File("2024/inputs/day03.txt").readText()).solve1().also {
+        check(it == 161289189) { "Invalid: $it" }
+        println(it)
+    }
+    Day03(testInput2).solve2().also {
+        check(it == 48) { "Invalid: $it" }
+        println(it)
+    }
+    Day03(File("2024/inputs/day03.txt").readText()).solve2().also {
+        check(it == 83595109) { "Invalid: $it" }
+        println(it)
+    }
+}
--- README.md
+++ README.md
@@ -13,13 +13,16 @@ My solutions to the [Advent of Code](https://adventofcode.com/) puzzles.
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2024 - 4 ⭐
+  2024 - 6 ⭐
 </h1>
 <a href="2024/kotlin/Day01.kt">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
 </a>
 <a href="2024/kotlin/Day02.kt">
   <img src=".aoc_tiles/tiles/2024/02.png" width="161px">
+</a>
+<a href="None">
+  <img src=".aoc_tiles/tiles/2024/03.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐
